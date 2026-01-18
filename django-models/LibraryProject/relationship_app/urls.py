54bc5682eb56e2f0d4576d6abd6c5789c889daa5
Eliud Mwaura <eliudmwaura101@gmail.com>
2026-01-18 21:49:53 +0300

    Implement custom permissions for Book model
    - Added Meta class to Book model with can_add_book, can_change_book, and can_delete_book.
    - Created secured views for adding, editing, and deleting books.
    - Applied @permission_required decorators to enforce granular access.
    - Configured URL patterns for all new secured book management actions.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -13,4 +13,9 @@ urlpatterns = [
     path('admin_view/', views.admin_view, name='admin_view'),
     path('librarian_view/', views.librarian_view, name='librarian_view'),
     path('member_view/', views.member_view, name='member_view'),
+    
+    # Adding, Editing and Deleting books URLs
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.detail import DetailView
 from .models import Library, Book
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 
 # Function-based view to list all books
 def list_books(request):
@@ -52,3 +53,24 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
+# View to add a book
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # Logic for adding a book
+    return render(request, 'relationship_app/add_book.html')
+
+# View to edit a book
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # Logic for editing the book 
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
+
+# View to delete a book
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # Logic for deleting the book
+    return render(request, 'relationship_app/delete_book.html', {'book': book})
