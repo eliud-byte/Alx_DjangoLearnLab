be46ba0dbf081095905bb985938471a2da1d2fcc
Eliud Mwaura <eliudmwaura101@gmail.com>
2026-02-05 23:30:11 +0300

    fix: adjust OrderingFilter reference to meet setup requirements
    - Updated api/views.py to use filters.OrderingFilter syntax
    - Standardized filter module imports for compatibility with project checks

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters import rest_framework as filters
-from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework as django_filters
+from rest_framework import filters
 from .models import Book
 from .serializers import BookSerializer
 
@@ -19,7 +19,11 @@ class BookListView(generics.ListAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     # Define the backends for filtering, searching, and ordering
-    filter_backends = [filters.DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [
+        django_filters.DjangoFilterBackend, 
+        filters.SearchFilter, 
+        filters.OrderingFilter
+    ]
 
     #Filtering: Exact matches or specified fields
     filterset_fields = ['title', 'author', 'publication_year']
